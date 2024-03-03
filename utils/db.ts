6347139f5457435df6f8704b00a1ff45de15f9de
silloi <silloi1993@gmail.com>
2024-03-04 01:22:16 +0900

    create db crud operations

--- utils/db.ts
+++ utils/db.ts
@@ -1,4 +1,5 @@
 // Copyright 2023-2024 the Deno authors. All rights reserved. MIT license.
+import { ulid } from "$std/ulid/mod.ts";
 
 const DENO_KV_PATH_KEY = "DENO_KV_PATH";
 let path = undefined;
@@ -9,6 +10,149 @@ if (
   path = Deno.env.get(DENO_KV_PATH_KEY);
 }
 export const kv = await Deno.openKv(path);
+
+/**
+ * Returns an array of values of a given {@linkcode Deno.KvListIterator} that's
+ * been iterated over.
+ *
+ * @example
+ * ```ts
+ * import { collectValues, listItems, type Item } from "@/utils/db.ts";
+ *
+ * const items = await collectValues<Item>(listItems());
+ * items[0].id; // Returns "01H9YD2RVCYTBVJEYEJEV5D1S1";
+ * items[0].userLogin; // Returns "snoop"
+ * items[0].title; // Returns "example-title"
+ * items[0].publishDate; // Returns 2023-12-31
+ * items[0].paragraphs; // Returns ["example content 1", "example content 2", "example content 3", "example content 4", "example content 5", "example content 6"]
+ * ```
+ */
+export async function collectValues<T>(iter: Deno.KvListIterator<T>) {
+  return await Array.fromAsync(iter, ({ value }) => value);
+}
+
+// Item
+export interface Item {
+  // Uses ULID
+  id: string;
+  userLogin: string;
+  title: string;
+  publishDate: string;
+  paragraphs: string[];
+}
+
+/** For testing */
+export function randomItem(): Item {
+  return {
+    id: ulid(),
+    userLogin: crypto.randomUUID(),
+    title: crypto.randomUUID(),
+    publishDate: Temporal.Now.plainDateISO().toString(),
+    paragraphs: [
+      crypto.randomUUID(),
+      crypto.randomUUID(),
+      crypto.randomUUID(),
+      crypto.randomUUID(),
+      crypto.randomUUID(),
+      crypto.randomUUID(),
+    ],
+  };
+}
+
+/**
+ * Creates a new item in the database. Throws if the item already exists in
+ * one of the indexes.
+ *
+ * @example
+ * ```ts
+ * import { createItem } from "@/utils/db.ts";
+ * import { ulid } from "$std/ulid/mod.ts";
+ *
+ * await createItem({
+ *   id: ulid(),
+ *   userLogin: "john_doe",
+ *   publishDate: "2023-12-31",
+ *   paragraphs: ["example content 1", "example content 2", "example content 3", "example content 4", "example content 5", "example content 6"],
+ * });
+ * ```
+ */
+export async function createItem(item: Item) {
+  const itemsKey = ["items", item.id];
+  const itemsByUserKey = ["items_by_user", item.userLogin, item.id];
+
+  const res = await kv
+    .atomic()
+    .check({ key: itemsKey, versionstamp: null })
+    .check({ key: itemsByUserKey, versionstamp: null })
+    .set(itemsKey, item)
+    .set(itemsByUserKey, item)
+    .commit();
+
+  if (!res.ok) throw new Error("Failed to create item");
+}
+
+/**
+ * Gets the item with the given ID from the database.
+ *
+ * @example
+ * ```ts
+ * import { getItem } from "@/utils/db.ts";
+ *
+ * const item = await getItem("01H9YD2RVCYTBVJEYEJEV5D1S1");
+ * item?.id; // Returns "01H9YD2RVCYTBVJEYEJEV5D1S1";
+ * item?.userLogin; // Returns "snoop"
+ * item?.title; // Returns "example-title"
+ * item?.url; // Returns "http://example.com"
+ * item?.score; // Returns 420
+ * ```
+ */
+export async function getItem(id: string) {
+  const res = await kv.get<Item>(["items", id]);
+  return res.value;
+}
+
+/**
+ * Returns a {@linkcode Deno.KvListIterator} which can be used to iterate over
+ * the items in the database, in chronological order.
+ *
+ * @example
+ * ```ts
+ * import { listItems } from "@/utils/db.ts";
+ *
+ * for await (const entry of listItems()) {
+ *   entry.value.id; // Returns "01H9YD2RVCYTBVJEYEJEV5D1S1"
+ *   entry.value.userLogin; // Returns "pedro"
+ *   entry.key; // Returns ["items_voted_by_user", "01H9YD2RVCYTBVJEYEJEV5D1S1", "pedro"]
+ *   entry.versionstamp; // Returns "00000000000000010000"
+ * }
+ * ```
+ */
+export function listItems(options?: Deno.KvListOptions) {
+  return kv.list<Item>({ prefix: ["items"] }, options);
+}
+
+/**
+ * Returns a {@linkcode Deno.KvListIterator} which can be used to iterate over
+ * the items by a given user in the database, in chronological order.
+ *
+ * @example
+ * ```ts
+ * import { listItemsByUser } from "@/utils/db.ts";
+ *
+ * for await (const entry of listItemsByUser("pedro")) {
+ *   entry.value.id; // Returns "01H9YD2RVCYTBVJEYEJEV5D1S1"
+ *   entry.value.userLogin; // Returns "pedro"
+ *   entry.key; // Returns ["items_voted_by_user", "01H9YD2RVCYTBVJEYEJEV5D1S1", "pedro"]
+ *   entry.versionstamp; // Returns "00000000000000010000"
+ * }
+ * ```
+ */
+export function listItemsByUser(
+  userLogin: string,
+  options?: Deno.KvListOptions,
+) {
+  return kv.list<Item>({ prefix: ["items_by_user", userLogin] }, options);
+}
 
 // User
 export interface User {
@@ -36,7 +180,8 @@ export async function createUser(user: User) {
   const usersKey = ["users", user.login];
   const usersBySessionKey = ["users_by_session", user.sessionId];
 
-  const atomicOp = kv.atomic()
+  const atomicOp = kv
+    .atomic()
     .check({ key: usersKey, versionstamp: null })
     .check({ key: usersBySessionKey, versionstamp: null })
     .set(usersKey, user)
@@ -66,7 +211,8 @@ export async function updateUserSession(user: User, sessionId: string) {
   const newUserBySessionKey = ["users_by_session", sessionId];
   const newUser: User = { ...user, sessionId };
 
-  const atomicOp = kv.atomic()
+  const atomicOp = kv
+    .atomic()
     .set(userKey, newUser)
     .delete(oldUserBySessionKey)
     .check({ key: newUserBySessionKey, versionstamp: null })
